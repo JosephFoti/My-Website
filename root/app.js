32cc34092fafbe67f4e96f51b030e79ab2257993
Joseph Foti <eljofo@gmail.com>
2018-07-03 10:41:09 -0400

    switching machines

--- root/app.js
+++ root/app.js
@@ -3,16 +3,19 @@ const http = require('http');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const Sequelize = require('sequelize')
+const cookieParser = require('cookie-parser');
+const Sequelize = require('sequelize');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const multer = require('multer');
+
 // const passport = require('passport');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('public'));
 
 
@@ -54,20 +57,91 @@ const Project = sequelize.define('project', {
 
 sequelize.sync()
 
-User.create({
+// User.create({
+//
+//   username: "AdminJoe",
+//   password: "password"
+//
+// });
 
-  username: "AdminJoe",
-  password: "password"
+
+
+// ----------------------------------------------------------------------------- PASSPORT JS INIT
+
+
+app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+passport.use(new Strategy(
+
+  (username, password, cb) => {
+    // NOTE User / Password confirmation for passportJS login
+    // use squelize search for first data entry with username feild match
+    User.findOne({
+      where: {
+        username: {
+          $iLike: `${username}`
+        }
+      }
+    }).then(data => {
+      if (!data) {
+        return cb(null, false);
+      } else if (data.password !== password) {
+        return cb(null, false);
+      }
+      return cb(null, data);
+    });
+  }
+
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, cb) {
+  // NOTE?? gets user data from previously defined local strategy, pushes to
+  // user parameter. first callback param is error throw?
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+
+  User.findById(id).then(data => {
+    if (!data) {
+      return cb(null, null);
+    }
+    cb(null, data);
+  });
 
 });
 
+// ------------------------------------------------ Multer
+
+
+
+// STORAGE OBJECT DEFINITION
+
+const storage = multer.diskStorage({
+destination: './public/images/development-portfolio',
+filename: (req, file, cb)=>{
+   cb(null, Date.now() + (file.originalname) );
+}
+})
+
+
+// UPLOAD PROCESS DEFINITION
+
+const upload = multer({storage: storage}).array('images')
 
 
 
 
 
-
-
+// ------------------------------------------------ Routes
 
 
 app.get('/', (req, res) => {
@@ -146,17 +220,64 @@ app.get('/portfolio/web-design/:projectName', (req,res)=>{
 
 });
 
-app.get('/admin', (req,res)=>{
-
-  res.render('login');
-
-})
-
 app.get('/front-end-development',(req,res)=>{
 
 
 
 })
+
+app.get('/admin', (req,res)=>{
+
+  res.render('login');
+
+});
+
+app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), (req,res)=>{
+
+  res.redirect('/admin-dashboard')
+
+})
+
+app.get('/admin-dashboard', (req,res)=>{
+
+  res.render('dashboard');
+
+});
+
+app.post('/add', (req,res)=>{
+
+  upload(req, res, (err)=>{
+    let order = req.body.order;
+    console.log(req.files);
+
+    let thisProject = {}
+
+    let orderedElements = [];
+
+    for (var i = 0; i < order.length; i++) {
+      if ( order[i] === 'img' ) {
+        let html = `<img src="${req.files[0].path}" class="dev-image" />`
+        req.files.shift();
+      } else {
+        let html = `<p class="dev-text">${req.body.text[0]}</p>`
+      }
+    }
+    // var x = '<div style="width:50px;height:50px;background-color:red"></div>';
+    // var y = '<div style="width:50px;height:50px;background-color:blue"></div>';
+    // var z = '<div style="width:50px;height:50px;background-color:green"></div>';
+
+    res.render('test',{orderedElements});
+
+  })
+
+
+
+
+})
+
+
+
+
 
 
 
